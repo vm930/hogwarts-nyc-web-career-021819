326a07af2563b30abc85943c41785374d68e018a
Victoria Mei <vm930@hotmail.com>
2019-04-26 13:50:33 -0400

    ui

--- src/components/Hogdex.js
+++ src/components/Hogdex.js
@@ -5,12 +5,11 @@ class Hogdex extends Component {
 	state = {
 		hogs: this.props.hogs
 	};
+
+	hog = () => this.props.hogs.map((hog) => <WholeHog {...hog} />); //spread out the entire hog obj into components
+
 	render() {
-		return (
-			<div>
-				<WholeHog />
-			</div>
-		);
+		return <div>{this.hog()}</div>;
 	}
 }
 
